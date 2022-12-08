1484c3f1f95f07b6fa0bc6d3d30b0f3c2ec1417a
Silvio <silvio.pilato@hudl.com>
2022-12-08 15:17:34 +0100

    Add basic css animation

--- src/components/Glass/index.tsx
+++ src/components/Glass/index.tsx
@@ -7,9 +7,9 @@ export type Props = {
 }
 
 export const Glass: React.FC<Props> = ({ingredients}) => {
-    let parts = 0;
+    let totalParts = 0;
     ingredients?.forEach((value) => {
-        parts+=value.part;
+        totalParts+=value.part;
     });
 
     return (
@@ -17,20 +17,30 @@ export const Glass: React.FC<Props> = ({ingredients}) => {
             <div className='glass-body'>
                 <div className='glass-ingredients'>
                     {
-                        ingredients?.map((ingredient) => {
-                            const height = `${ingredient.part / parts * 100}%`;
+                        ingredients?.map((ingredient, index) => {
+                            const height = `${ingredient.part / totalParts * 100}%`;
                             const pluralS = ingredient.part != 1 ? "s" : "" ;
                             const text = `${ingredient.part} part${pluralS} of `;
+                            const animationDuration = 0.5;
+                            const animationDelay = animationDuration * (ingredients.length - index - 1);
+                            /** 
+                             * We make a random index so that react will always  
+                             * recreate the whole divthe animation will always work
+                            **/
+                            const elIndex = `${ingredient.name}-${Math.random()}`;
+
                             return (
-                                <div className='ingredient-text' style={{
+                                <div className='ingredient-text-wrapper' key={elIndex} style={{
                                     backgroundColor: ingredient.color,
                                     width: '100%',
                                     height,
                                     display: "flex",
                                     alignItems: "center",
-                                    justifyContent: "center"
+                                    justifyContent: "center",
+                                    animationDuration: `${animationDuration}s`, 
+                                    animationDelay: `${animationDelay}s`
                                 }}>
-                                    <div>
+                                    <div className='ingredient-text'>
                                         {text}
                                         <span className='ingredient-name ingredient-text'>{ingredient.name}</span>
                                     </div>
