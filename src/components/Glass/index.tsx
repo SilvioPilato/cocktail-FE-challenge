9aea6f52bbc4dd4f261a8ba2380a8cd6d547951a
Silvio <silvio.pilato@hudl.com>
2022-12-08 18:10:24 +0100

    Add glass base

--- src/components/Glass/index.tsx
+++ src/components/Glass/index.tsx
@@ -13,44 +13,47 @@ export const Glass: React.FC<Props> = ({ingredients}) => {
     });
 
     return (
-        <div className='glass-wrapper'>
-            <div className='glass-body'>
-                <div className='glass-ingredients'>
-                    {
-                        ingredients?.map((ingredient, index) => {
-                            const height = `${ingredient.part / totalParts * 100}%`;
-                            const pluralS = ingredient.part != 1 ? "s" : "" ;
-                            const text = `${ingredient.part} part${pluralS} of `;
-                            const animationDuration = 0.5;
-                            const animationDelay = animationDuration * (ingredients.length - index - 1);
-                            /** 
-                             * We make a random index so that react will always  
-                             * recreate the whole divthe animation will always work
-                            **/
-                            const elIndex = `${ingredient.name}-${Math.random()}`;
+        <div>
+            <div className='glass-wrapper'>
+                <div className='glass-body'>
+                    <div className='glass-ingredients'>
+                        {
+                            ingredients?.map((ingredient, index) => {
+                                const height = `${ingredient.part / totalParts * 100}%`;
+                                const pluralS = ingredient.part != 1 ? "s" : "" ;
+                                const text = `${ingredient.part} part${pluralS} of `;
+                                const animationDuration = 0.5;
+                                const animationDelay = animationDuration * (ingredients.length - index - 1);
+                                /** 
+                                 * We make a random index so that react will always  
+                                 * recreate the whole divthe animation will always work
+                                **/
+                                const elIndex = `${ingredient.name}-${Math.random()}`;
 
-                            return (
-                                <div className='ingredient-text-wrapper' key={elIndex} style={{
-                                    backgroundColor: ingredient.color,
-                                    width: '100%',
-                                    height,
-                                    display: "flex",
-                                    alignItems: "center",
-                                    justifyContent: "center",
-                                    animationDuration: `${animationDuration}s`, 
-                                    animationDelay: `${animationDelay}s`
-                                }}>
-                                    <div className='ingredient-text'>
-                                        {text}
-                                        <span className='ingredient-name ingredient-text'>{ingredient.name}</span>
+                                return (
+                                    <div className='ingredient-text-wrapper' key={elIndex} style={{
+                                        backgroundColor: ingredient.color,
+                                        width: '100%',
+                                        height,
+                                        display: "flex",
+                                        alignItems: "center",
+                                        justifyContent: "center",
+                                        animationDuration: `${animationDuration}s`, 
+                                        animationDelay: `${animationDelay}s`
+                                    }}>
+                                        <div className='ingredient-text'>
+                                            {text}
+                                            <span className='ingredient-name ingredient-text'>{ingredient.name}</span>
+                                        </div>
                                     </div>
-                                </div>
-                            )
-                        })
-                    }
-                    
+                                )
+                            })
+                        }
+                        
+                    </div>
                 </div>
             </div>
+            <hr className='glass-base'/>
         </div>
     )
 }
